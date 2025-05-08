f84f51f03f743b3cfd8f7a36ec190e741d0fac24
kikak25 <kkovaleski@smith.edu>
2025-05-08 16:14:06 -0400

    Update moon_light_join.sql
    Updated header names

--- moon_light_join.sql
+++ moon_light_join.sql
@@ -1,10 +1,10 @@
 /* CREATE TABLE `MacLeish` (
-  `YYYY-MM-DDTHH:mm:ss.fff` text,
-  `YYYY-MM-DDTHH:mm:ss.fff_[0]` text,
-  `Celsius` double DEFAULT NULL,
-  `number` double DEFAULT NULL,
-  `Hz` double DEFAULT NULL,
-  `mag/arcsec^2` double DEFAULT NULL
+  `UTC_Date_And_Time` text,
+  `Local_Date_And_Time` text,
+  `Temperature` double DEFAULT NULL,
+  `Counts` double DEFAULT NULL,
+  `Frequency` double DEFAULT NULL,
+  `MSAS` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `moon_phases` (
@@ -27,4 +27,4 @@ IGNORE 1 LINES; */
 SELECT *
 FROM mothitor_light.MacLeish
 INNER JOIN mothitor_light.moon_phases
-ON mothitor_light.moon_phases.Date = SUBSTRING(`YYYY-MM-DDTHH:mm:ss.fff`, 1, 10);
+ON mothitor_light.moon_phases.Date = SUBSTRING(`UTC_Date_And_Time`, 1, 10);
